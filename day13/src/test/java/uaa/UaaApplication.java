f8fd2ec9fd8320ab80b94801d14b913f82d9c3bc
MrLiPeter <599490685@qq.com>
2022-05-15 15:22:00 +0800

    4.7 自动化测试

---
+++ day13/src/test/java/uaa/UaaApplication.java
@@ -0,0 +1,10 @@
+package uaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UaaApplication {
+    @Test
+    void contextLoads(){}
+}
